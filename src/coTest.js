7a533b93394b081ae2fb48ad01eeff19e165cd3f
Ulzahk <ulzahk@gmail.com>
2020-11-07 18:03:11 -0500

    [CREATED] Subclasses for each unique car insurance type

--- src/coTest.js
+++ src/coTest.js
@@ -12,56 +12,102 @@ class CarInsurance {
   }
   updatePrice() {
     for (var i = 0; i < this.products.length; i++) {
-      if (this.products[i].name != 'Full Coverage' && this.products[i].name != 'Special Full Coverage') {
-        if (this.products[i].price > 0) {
-          if (this.products[i].name != 'Mega Coverage') {
-            this.products[i].price = this.products[i].price - 1;
-          }
-        }
-      } else {
-        if (this.products[i].price < 50) {
-          this.products[i].price = this.products[i].price + 1;
-          if (this.products[i].name == 'Special Full Coverage') {
-            if (this.products[i].sellIn < 11) {
-              if (this.products[i].price < 50) {
-                this.products[i].price = this.products[i].price + 1;
-              }
-            }
-            if (this.products[i].sellIn < 6) {
-              if (this.products[i].price < 50) {
-                this.products[i].price = this.products[i].price + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.products[i].name != 'Mega Coverage') {
-        this.products[i].sellIn = this.products[i].sellIn - 1;
-      }
-      if (this.products[i].sellIn < 0) {
-        if (this.products[i].name != 'Full Coverage') {
-          if (this.products[i].name != 'Special Full Coverage') {
-            if (this.products[i].price > 0) {
-              if (this.products[i].name != 'Mega Coverage') {
-                this.products[i].price = this.products[i].price - 1;
-              }
-            }
-          } else {
-            this.products[i].price = this.products[i].price - this.products[i].price;
-          }
+      if (this.products[i].price > 0) {
+        if(this.products[i].sellIn > 0) {
+            this.products[i].price --
         } else {
-          if (this.products[i].price < 50) {
-            this.products[i].price = this.products[i].price + 1;
-          }
+          this.products[i].price -= 2
         }
       }
+      this.products[i].sellIn --
     }
+    return this.products
+  }
+}
 
-    return this.products;
+
+class FullCoverage extends CarInsurance {
+  constructor(products = []) {
+    super(products);
+  }
+  updatePrice() {
+    for (var i = 0; i < this.products.length; i++) {
+      if (this.products[i].price < 50) {
+        if(this.products[i].sellIn > 0) {
+            this.products[i].price ++
+        } else {
+          this.products[i].price += 2
+        }
+      }
+      this.products[i].sellIn --
+    }
+    return this.products
+  }
+}
+
+class MegaCoverage extends CarInsurance {
+  constructor(products = []){
+    super(products)
+  }
+  updatePrice() {
+    return this.products
+  }
+}
+
+class SpecialFullCoverage extends CarInsurance {
+  constructor(products = []){
+    super(products)
+  }
+  updatePrice() {
+      for (var i = 0; i < this.products.length; i++) {
+        if(this.products[i].sellIn > 0 ){
+          if(this.products[i].price < 50){
+            if(this.products[i].sellIn <= 5){
+              this.products[i].price === 49 ? 
+              this.products[i].price ++ : this.products[i].price === 48 ? 
+              this.products[i].price += 2 : this.products[i].price += 3
+            } else if (this.products[i].sellIn <= 10){
+              this.products[i].price === 49 ?
+              this.products[i].price ++ : this.products[i].price += 2
+            } else {
+              this.products[i].price ++
+            }
+          }
+        } else{
+          this.products[i].price *= 0
+        }
+      this.products[i].sellIn --
+    }
+    return this.products
+  }
+}
+
+class SuperSale extends CarInsurance {
+  constructor(products = []){
+    super(products)
+  }
+  updatePrice() {
+    for (var i = 0; i < this.products.length; i++) {
+      this.products[i].price === 1 ?
+      this.products[i].price -- : this.products[i].price
+      if (this.products[i].price > 0) {
+        if(this.products[i].sellIn > 0) {
+          this.products[i].price -= 2
+        } else {
+          this.products[i].price -= 4
+        }
+      }
+      this.products[i].sellIn --
+    }
+    return this.products
   }
 }
 
 module.exports = {
   Product,
-  CarInsurance
+  CarInsurance,
+  FullCoverage,
+  MegaCoverage,
+  SpecialFullCoverage,
+  SuperSale,
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,46 @@
-const { Product, CarInsurance }  = require('./coTest')
+const { Product, CarInsurance, FullCoverage, MegaCoverage, SpecialFullCoverage, SuperSale }  = require('./coTest')
 
 const productsAtDayZero = [
   new Product('Medium Coverage', 10, 20),
-  new Product('Full Coverage', 2, 0),
   new Product('Low Coverage', 5, 7),
+];
+
+const productsAtDayZeroFullCoverage = [
+  new Product('Full Coverage', 2, 0),
+]
+
+const productsAtDayZeroMegaCoverage = [
   new Product('Mega Coverage', 0, 80),
   new Product('Mega Coverage', -1, 80),
+]
+
+const productsAtDayZeroSpecialFullCoverage = [
   new Product('Special Full Coverage', 15, 20),
   new Product('Special Full Coverage', 10, 49),
   new Product('Special Full Coverage', 5, 49),
-  new Product('Super Sale', 3, 6),
-];
+]
 
-const carInsurance = new CarInsurance(productsAtDayZero);
-const productPrinter = function (product) {
-  console.log(`${product.name}, ${product.sellIn}, ${product.price}`);
+const productsAtDayZeroSuperSalce = [
+  new Product('Super Sale', 3, 6),
+]
+
+const carInsurance = new CarInsurance(productsAtDayZero)
+const fullCoverage = new FullCoverage(productsAtDayZeroFullCoverage)
+const megaCoverage = new MegaCoverage(productsAtDayZeroMegaCoverage)
+const specialFullCoverage = new SpecialFullCoverage(productsAtDayZeroSpecialFullCoverage)
+const superSale = new SuperSale(productsAtDayZeroSuperSalce)
+
+const productPrinter = (product) => {
+  console.log(`${product.name}, ${product.sellIn}, ${product.price}`)
 };
 
-for (let i = 1; i <= 30; i += 1) {
-  console.log(`-------- day ${i} --------`);
-  console.log('name, sellIn, price');
-  carInsurance.updatePrice().forEach(productPrinter);
-  console.log('');
+for (let i = 1; i <= 30; i ++) {
+  console.log(`-------- day ${i} --------`)
+  console.log('name, sellIn, price')
+  carInsurance.updatePrice().forEach(productPrinter)
+  fullCoverage.updatePrice().forEach(productPrinter)
+  megaCoverage.updatePrice().forEach(productPrinter)
+  specialFullCoverage.updatePrice().forEach(productPrinter)
+  superSale.updatePrice().forEach(productPrinter)
+  console.log('')
 }
